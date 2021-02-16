9536fcd8d729be09f0008172a3c16955f1cdbe48
Ed <erucquoy@gmail.com>
2021-02-16 09:51:58 +0100

    starter pack

---
+++ ex01/db.php
@@ -0,0 +1,4 @@
+<?php 
+// connexion a la db
+
+?>
---
+++ ex01/db_utils.php
@@ -0,0 +1,25 @@
+<?php 
+
+/*
+1) Créer une fonction qui récupère tous les produits.
+2) Créer une fonction qui récupère toutes les catégories.
+3) Créer une fonction qui récupère les produits d'une catégorie
+
+
+Infos importantes:
+- Utilisez PDO, les fonctions `prepare` et `execute` de PDO (et bindValue si besoin).
+*/
+function getAllProducts() {
+
+    return [];
+}
+
+function getAllCategories() {
+
+    return [];
+}
+
+function getProductsByCategory($id) {
+
+    return [];
+}
